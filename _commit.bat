00d0d5a4aa0f4bf301fc270eb0802e948f9752dd
Vincentlibinxin <kongfuzhi0415@gmail.com>
2026-04-24 00:58:42 +0800

    chore: remove temp commit script

--- _commit.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "c:\Users\kongf\Desktop\集运系统\JIYUN-ADMIN"
-git add -A
-git commit -m "feat(v0.2.15): add soft-delete column; default filter excludes deleted records"
-git tag v0.2.15
-git push
-git push --tags
